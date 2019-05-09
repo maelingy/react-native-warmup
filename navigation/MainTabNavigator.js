60a5ed310b0efb458d2b22a13521b0e797b1e615
maelingy <yipmaeling@gmail.com>
2019-05-09 14:53:00 -0700

    first commit

---
+++ navigation/MainTabNavigator.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import { Platform } from 'react-native';
+import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+
+import TabBarIcon from '../components/TabBarIcon';
+import HomeScreen from '../screens/HomeScreen';
+import FetchExample from '../screens/FetchExample';
+import ApolloExample from '../screens/ApolloExample';
+import SettingsScreen from '../screens/SettingsScreen';
+
+const HomeStack = createStackNavigator({
+  Home: HomeScreen,
+});
+
+HomeStack.navigationOptions = {
+  tabBarLabel: 'Home',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={
+        Platform.OS === 'ios'
+          ? `ios-information-circle${focused ? '' : '-outline'}`
+          : 'md-information-circle'
+      }
+    />
+  ),
+};
+
+const FetchStack = createStackNavigator({
+  Fetch: FetchExample,
+});
+
+FetchStack.navigationOptions = {
+  tabBarLabel: 'Fetch',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'}
+    />
+  ),
+};
+
+const ApolloStack = createStackNavigator({
+  Apollo: ApolloExample,
+});
+
+ApolloStack.navigationOptions = {
+  tabBarLabel: 'Apollo',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'}
+    />
+  ),
+};
+
+const SettingsStack = createStackNavigator({
+  Settings: SettingsScreen,
+});
+
+SettingsStack.navigationOptions = {
+  tabBarLabel: 'Settings',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'}
+    />
+  ),
+};
+
+export default createBottomTabNavigator({
+  HomeStack,
+  FetchStack,
+  ApolloStack,
+  SettingsStack,
+});
---
+++ screens/ApolloExample.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { FlatList, ActivityIndicator, Text, View, Image  } from 'react-native';
+
+export default class ApolloExample extends React.Component {
+  state = {
+    isLoading: true,
+    shibes: [],
+    error: null
+  };
+  fetchUsers() {
+    fetch(`http://shibe.online/api/shibes?count=[1-100]&urls=[true]&httpsUrls=[true]`)
+      .then(response => response.json())
+      .then(data =>
+        this.setState({
+          shibes: data,
+          isLoading: false,
+        })
+      )
+      .catch(error => this.setState({ error, isLoading: false }));
+  }
+
+  componentDidMount() {
+    this.fetchUsers();
+  }
+  render() {
+    const { isLoading, shibes, error } = this.state;
+    return (
+      <React.Fragment>
+        <Text>Random Shibe Generator</Text>
+        {error ? <p>{error.message}</p> : null}
+        {!isLoading ? (
+          shibes.map(shibe => {
+            const {  } = shibe;
+            return (
+              <View key={""}>
+                <Image source={{uri: 'https://cdn.shibe.online/shibes/' + shibe + '.jpg'}}
+                  style={{width: 400, height: 400}} />
+              </View>
+            );
+          })
+        ) : (
+          <Text>Loading...</Text>
+        )}
+      </React.Fragment>
+    );
+  }
+}
+
